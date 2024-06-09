684e1383208df5fc5231c3a2cfea713842ae2566
Chuchi254 <samsongikonyo142@gmai.com>
2024-06-08 23:04:06 -0700

    checks if binary tree is complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - Checksif abinary tree is complete
+ *
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the treee is complete, otherwise 0
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1024);
+	int front = 0, rear = 0, found_null = 0;
+
+	if (queue == NULL)
+		return (0);
+
+	queue[rear++] = (binary_tree_t *)tree;
+
+	while (front < rear)
+	{
+		binary_tree_t *current = queue[front++];
+
+		if (current == NULL)
+		{
+			found_null = 1;
+		}
+		else
+		{
+			if (found_null)
+			{
+				free(queue);
+				return (0);
+			}
+			queue[rear++] = current->left;
+			queue[rear++] = current->right;
+		}
+	}
+	free(queue);
+	return (1);
+}
